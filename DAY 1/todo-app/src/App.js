97838bbaf44a1352fedc2294229625854c317702
KaptainCS3 <mbiapplegryn@gmail.com>
2023-01-28 21:36:52 +0100

    import moduels

--- DAY 1/todo-app/src/App.js
+++ DAY 1/todo-app/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
-
+import MainContainer from "./components/MainContainer";
+import SideBar from "./components/SideBar"
 const App = () => {
   return (
-    <h1>Hello Redux</h1>
+    <main className="flex justify-center items-center h-screen">
+      <div className="App__container flex p-1.5 bg-[#fff] rounded-[.75em]">
+        <SideBar />
+        <MainContainer />
+      </div>
+    </main>
   );
 };
 
