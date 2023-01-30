45c58ac8cd624734ec70d184438716d87bcadc78
KaptainCS3 <mbiapplegryn@gmail.com>
2023-01-30 15:11:02 +0100

    added comment description

--- DAY 1/todo-app/src/app/store.js
+++ DAY 1/todo-app/src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "../features/Todos/todoSlice";
+//! create a reducer store for app global state
 export const store = configureStore({
   reducer: {
     completeTask: todoReducer,
