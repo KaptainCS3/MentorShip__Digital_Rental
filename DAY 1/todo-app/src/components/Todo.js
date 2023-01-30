a334f7aec0a0da3d81f2dc49d85c72474b6ae92f
KaptainCS3 <mbiapplegryn@gmail.com>
2023-01-30 15:20:45 +0100

    added comment description

--- DAY 1/todo-app/src/components/Todo.js
+++ DAY 1/todo-app/src/components/Todo.js
@@ -1,42 +1,54 @@
-import { faCheck, faPen, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+//! hook to pass reducer function to component
 import { useDispatch } from "react-redux";
-import { removeTask } from "../features/Todos/todoSlice";
-import React from "react";
+import {  removeTask } from "../features/Todos/todoSlice";
+import React, { useState } from "react";
+import Slide from "react-reveal";
 
 const Todo = (props) => {
-  // console.log();
   const dispatch = useDispatch();
-  const handleClick = () => dispatch(removeTask(props.todos.id));
+
+  //! mark complete task 
+  const [mark, setMark] = useState(props.isComplete);
+  //! event handler mark task done
+  const handleMark = () => {
+    setMark((prevMark) => {
+      return !prevMark;
+    });
+  };
+  //! remove task from Todolist handler
+  const handleRemove = () => dispatch(removeTask({ id: props.id }));
+
   return (
+    <Slide left>
     <section className="mt-2 mb-12 mr-2">
-      <div className="flex rounded-[.75em] bg-[#fff] h-[45px] shadow-none">
+      <div className="flex rounded-[.75em] bg-[#fff] h-[auto] min-h-[2.8em] shadow-none">
         <div className="flex flex-row justify-between w-full items-center px-8">
-          <div>{props.todos.task}</div>
+          //! ternary operator condition check
+          <div style={{ textDecoration: mark ? "line-through" : "" }}>
+            {props.task}
+          </div>
           <section className="flex w-[15%] justify-between">
             <div>
               <FontAwesomeIcon
                 icon={faCheck}
                 className="cursor-pointer text-green-600"
+                onClick={handleMark}
               />
             </div>
             <div>
               <FontAwesomeIcon
                 icon={faXmark}
                 className="cursor-pointer text-red-600"
-                onClick={handleClick}
-              />
-            </div>
-            <div>
-              <FontAwesomeIcon
-                icon={faPen}
-                className="cursor-pointer text-[#717082]"
+                onClick={handleRemove}
               />
             </div>
           </section>
         </div>
       </div>
     </section>
+    </Slide>
   );
 };
 
