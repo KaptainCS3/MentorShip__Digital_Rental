1dacf0b77aee24c7d347011e2e7cfac64331d57f
KaptainCS3 <mbiapplegryn@gmail.com>
2023-01-30 15:34:41 +0100

    added media query class

--- DAY 1/todo-app/src/App.js
+++ DAY 1/todo-app/src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import MainContainer from "./components/MainContainer";
 import SideBar from "./components/SideBar"
 const App = () => {
+
   return (
-    <main className="flex justify-center items-center h-screen">
-      <div className="App__container flex p-1.5 bg-[#fff] rounded-[.75em]">
+    <main className="flex justify-center items-center h-screen app__h">
+      <div className="App__container flex p-1.5 bg-[#fff] rounded-[.75em] direction">
         <SideBar />
         <MainContainer />
       </div>
